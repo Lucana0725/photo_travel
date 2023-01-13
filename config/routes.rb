5c63808ea671d312f645b9845a91600de3fb2785
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2023-01-13 14:14:02 +0000

    [Finish]Devise関係のルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,20 @@ Rails.application.routes.draw do
   root to: 'public/homes#top'
   get 'about' => 'public/homes#about'
   
-  devise_for :admins
-  devise_for :users
+  # ユーザー用
+  # URL /users/sign_in ...
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  # devise_for :admins
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
