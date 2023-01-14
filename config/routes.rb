953d94cb69b19687c84ba61dbeb8dc813c95581e
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-01-14 14:53:13 +0000

    [Finish]userのログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   
+  # namespace :admin do
+  #   root to: 'homes#top'
+  # end
+  
   # devise_for :admins
   # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
