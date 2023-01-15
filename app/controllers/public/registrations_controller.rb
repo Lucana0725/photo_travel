8b9cfbfaf923f837a753281f689544ec9a1689e3
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-01-15 08:00:43 +0000

    [Fixed]ユーザー新規登録時のデータ送信エラーの修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_params, if: :devise_controller?
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  def after_sign_up_path_for(resource)
-    about_path
-  end
 
   # GET /resource/sign_up
   # def new
@@ -52,11 +50,19 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
+  # ユーザー新規登録時のDBへのデータ(カラム)送信を許可する内容
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
+  # 新規登録処理後の遷移先
+  def after_sign_up_path_for(resource)
+    about_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
