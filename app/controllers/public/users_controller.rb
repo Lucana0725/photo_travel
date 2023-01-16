78aefca872acc0858be72d4c5f33c887e47b9a8b
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-01-16 05:32:09 +0000

    [Finish]ユーザー情報の変更ができるように実装、画像が反映されない不具合を解消

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,14 +4,26 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    # byebug
+    @user = current_user
+    @user.update!(user_params)
+    redirect_to mypage_path
   end
 
   def unsubscribe
   end
 
   def withdrawal
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit([:profile_image, :nickname, :introduction])
   end
 end
