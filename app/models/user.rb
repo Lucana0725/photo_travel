78682d034becac920b9244bd843d15b97855f538
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-01-23 13:42:53 +0000

    [Finish]favorites#destroyの定義、viewページでいいねの表示関係の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          
   # アソシエーション
   has_many :travels, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   # プロフィール画像を:profile_imageにて保持
   has_one_attached :profile_image
