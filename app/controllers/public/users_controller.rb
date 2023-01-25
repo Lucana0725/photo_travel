0cef6da92710b4c55d44f81c59dbfb50c936aedc
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-01-25 09:19:10 +0000

    [Finish]users#favoritesアクションの定義、viewページ作成、users/showにusers/favoritesへのリンク設置

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,6 @@
 class Public::UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
-  
+
   def show
     @user = User.find(params[:id])
     @travels = @user.travels  # users#showでユーザー毎の全ての投稿を表示
@@ -31,15 +31,22 @@ class Public::UsersController < ApplicationController
     reset_session
     redirect_to root_path
   end
-  
-  
+
+  # いいね！一覧用アクション
+  def favorites
+    @user = User.find(params[:id])  # ユーザー情報を取得
+    favorites = Favorite.where(user_id: @user.id).pluck(:travel_id)
+    @favorite_travels = Travel.find(favorites)
+  end
+
+
   private
-  
+
   def user_params
     params.require(:user).permit([:profile_image, :nickname, :introduction])
   end
-  
-  
+
+
   # 他人のプロフィールを変更できないようにするバリデーション
   def is_matching_login_user
     user_id = params[:id].to_i
