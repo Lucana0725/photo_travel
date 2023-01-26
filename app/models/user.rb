b47c197063a14dc2f70a85b1f56d18b63c3ffda9
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-01-26 15:52:19 +0000

    [Finish]Userモデルにフォロー時の挙動のメソッド作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,27 @@ class User < ApplicationRecord
   has_many :following_user, through: :follower, source: :followed  # 自分がフォローしている人
   has_many :follower_user, through: :followed, source: :follower  # 自分をフォローしている人（自分がフォローされている人）
   
+  
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  
+  # フォローしているかの判定
+  def following?(user)
+    following_user.include?(user)
+  end
+  
+  
+  
+  
+  
   # プロフィール画像を:profile_imageにて保持
   has_one_attached :profile_image
   
