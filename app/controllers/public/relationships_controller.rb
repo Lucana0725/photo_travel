0deaf46354ed08abd4bfe33c67e3471437cbb750
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-01-27 12:28:37 +0000

    [Finish]public/relationshipsコントローラー作成、各種定義。ルーティング作成

---
+++ app/controllers/public/relationships_controller.rb
@@ -0,0 +1,21 @@
+class Public::RelationshipsController < ApplicationController
+  def create
+    current_user.follow(params[:user_id])  # Userモデルのfollowメソッドを参照している
+    redirect_to request.referer
+  end
+  
+  def destroy
+    current_user.unfollow(params[:user_id])  # Userモデルのunfollowメソッドを参照している
+    redirect_to request.referer
+  end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.following_user  # Userモデルのhas_manyの:following_user
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.follower_user  # Userモデルのhas_manyの:follower_user
+  end
+end
