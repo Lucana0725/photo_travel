a79d80bcca0f19ce7aa37316f001319e20992dfd
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-01-29 09:52:10 +0000

    [Finish]検索フォーム2種をabout, log_in, sign_upページで表示しないよう実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # app/views/layouts/application.html.erbに記述した検索フォームを、about, log_in, sign_upで表示したくないので、こちらでヘルパーメソッドとしてまとめて定義
+  def hide_search_bar?
+    current_page?(controller: 'public/sessions', action: 'new') ||
+      current_page?(controller: 'public/registrations', action: 'new') ||
+      current_page?(controller: 'public/homes', action: 'about')
+  end
 end
