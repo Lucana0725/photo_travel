13ef8bac748c9df206beceacf4f5ca1b35a6bec8
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-02-08 10:09:44 +0000

    [Finish]フォロー/フォロワー一覧のerbファイルのバックアップ作成、フォロー一覧ページのレイアウト作成

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,12 +3,12 @@ class Public::RelationshipsController < ApplicationController
     current_user.follow(params[:user_id])  # Userモデルのfollowメソッドを参照している
     redirect_to request.referer
   end
-  
+
   def destroy
     current_user.unfollow(params[:user_id])  # Userモデルのunfollowメソッドを参照している
     redirect_to request.referer
   end
-  
+
   def followings
     user = User.find(params[:user_id])
     @users = user.following_user  # Userモデルのhas_manyの:following_user
