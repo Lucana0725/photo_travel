3418b7d24a9f512b489a07a6258b090a31a53ae5
Lucana0725 <k.ark.scarlet72@gmail.com>
2023-02-11 09:53:19 +0000

    [Finish]layouts/application.html.erbの<header>をパーシャル化、各背景画像を充てたいviewをlayoutに別途作成、各自コントローラーにて読み込ませ別々の背景画像を表示するよう実装

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,10 @@
 class Public::HomesController < ApplicationController
   def top
     @travels = Travel.order('created_at DESC').limit(4)  # public/homes/top.html.backup.erb用
+    render layout: 'top'  # views/layouts/top.html.erbを読み込む。
   end
 
   def about
+    render layout: 'about'
   end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  layout 'sessions'
   def after_sign_in_path_for(resource)
     travels_path
   end
